80a0d4676f45acd4d3950edc702314a1772c87f4
AntoniaFuenzalida <afuenzalida20@alumnos.utalca.cl>
2025-06-01 18:09:04 -0400

    Conexión back y front
    Ahora se puede obtener, visualiza y eliminar a una mascota

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+// app.js
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -5,11 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Rutas
 const userRoutes = require('./routes/usuarios');
 const mascotaRoutes = require('./routes/mascotas');
 
-app.use('/api', userRoutes);
-app.use('/api', mascotaRoutes);
-
+app.use('/api', userRoutes);      // por ejemplo: /api/usuarios/login
+app.use('/api', mascotaRoutes);   // por ejemplo: /api/mascotas/dueno/:id_dueno
 
 module.exports = app;
