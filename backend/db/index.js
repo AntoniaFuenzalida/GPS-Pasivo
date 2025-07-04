5d85d01171a67917cd6bdd1a63ef9a706aafa908
lan-xd <rayox67@gmail.com>
2025-07-04 15:17:38 -0400

    MAs cambios

--- backend/db/index.js
+++ backend/db/index.js
@@ -6,11 +6,16 @@ const dbHost = process.env.DOCKERIZED === 'true'
   ? process.env.DB_HOST_INTERNAL 
   : process.env.DB_HOST_EXTERNAL;
 
+// Usa el puerto correspondiente según el host
+const dbPort = process.env.DOCKERIZED === 'true'
+  ? process.env.DATABASE_INTERNAL_PORT
+  : process.env.DATABASE_EXTERNAL_PORT;
+
 const pool = mysql.createPool({
   host: dbHost || process.env.DB_HOST || 'db',
   user: process.env.DB_USER || 'admin',
   password: process.env.DB_PASSWORD || 'Utalca_123',
-  port: process.env.DATABASE_INTERNAL_PORT || 9306,
+  port: dbPort || 3306,
   database: process.env.DB_NAME || 'gps',
   waitForConnections: process.env.DB_WAIT_FOR_CONNECTIONS === 'true',
   connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,
